cf21cf67c599de91a4fdfa77c925f130251fc956
ZHOU Lizhi <reasonsolo@gmail.com>
2021-08-29 20:01:49 +0800

    add uri_param

---
+++ service/uri_params.cc
@@ -0,0 +1,19 @@
+// author: zlz
+
+#include "service/uri_params.h"
+
+namespace grd {
+namespace service {
+
+const std::string kAppParam = "app";
+const std::string kDefaultApp = "default";
+const std::string kUri = "url";
+
+const std::string kAppId = "appid";
+const std::string kNonce = "nonce";
+const std::string kUser = "user";
+const std::string kTimestamp = "timestamp";
+const std::string kSha1 = "sha1";
+
+}  // namespace service
+}  // namespace grd
---
+++ service/uri_params.h
@@ -0,0 +1,19 @@
+// author:zlz
+#pragma once
+
+#include <string>
+
+namespace grd {
+namespace service {
+
+extern const std::string kAppParam;
+extern const std::string kDefaultApp;
+extern const std::string kUri;
+extern const std::string kAppId;
+extern const std::string kNonce;
+extern const std::string kUser;
+extern const std::string kTimestamp;
+extern const std::string kSha1;
+
+}  // namespace service
+}  // namespace grd
